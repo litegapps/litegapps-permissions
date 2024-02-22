46834cf4dc3c145225306c82f336c07865850994
wahyu6070 <gwtsendangrejo6070@gmail.com>
2024-02-22 17:41:45 +0700

    update

--- mapp.sh
+++ mapp.sh
@@ -4,20 +4,29 @@ getp(){ grep "^$1" "$2" | head -n1 | cut -d = -f 2; }
 BASE=`dirname $(readlink -f $0)`
 LOG=app.xml
 
-echo '<permissions>' > $LOG
-echo "  <privapp-permissions package='com.google.android.gms'>" >> $LOG
-for Y in $(aapt d permissions *.apk); do
+
+for YT in $(find $BASE/input -type f -name *apk); do
+PACKAGE_NAME=`aapt d permissions $YT | grep package | head -n1 | cut -d : -f 2 | sed 's/ //g'`
+
+OUT=$BASE/output/permissions/${PACKAGE_NAME}.xml
+echo " out : $OUT"
+rm -rf $BASE/output/permissions
+mkdir -p $BASE/output/permissions
+echo '<permissions>' > $OUT
+echo "  <privapp-permissions package='$PACKAGE_NAME'>" >> $OUT
+for Y in $(aapt d permissions $YT); do
 	
 	if [[ $Y == *name=* ]]; then
 		T=`echo "$Y" | head -n1 | cut -d = -f 2`
-		echo "    <permission name=${T} />" | tee -a $LOG
+		echo "    <permission name=${T} />" >> $OUT
 	elif [[ $Y != *name=* ]] && [[ $Y == *com.* ]]; then
 		T=`echo "$Y" | head -n1 | cut -d = -f 2`
-		echo "    <permission name='${T}' />" | tee -a $LOG
+		echo "    <permission name='${T}' />" >> $OUT
 	fi
 done
 
-echo "   </privapp-permissions>" >> $LOG
-echo "</permissions>" >> $LOG
+echo "   </privapp-permissions>" >> $OUT
+echo "</permissions>" >> $OUT
 
-sed -i 's/"'"/'"'/g' $LOG
+sed -i 's/"'"/'"'/g' $OUT
+done
